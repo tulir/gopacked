1c9746bf0266535ae7ca31d6ad64aac77f0346ec
Tulir293 <tulir293@maunium.net>
2016-02-06 20:58:37 +0200

    Remove HTTPGet as it was not used anywhere.

--- util.go
+++ util.go
@@ -2,25 +2,9 @@ package main
 
 import (
 	"io"
-	"io/ioutil"
 	"net/http"
 	"os"
 )
-
-// HTTPGet performs a HTTP GET request on the given URL
-func HTTPGet(url string) []byte {
-	response, err := http.Get(url)
-	if err != nil {
-		return []byte{}
-	}
-	defer response.Body.Close()
-	contents, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return []byte{}
-	}
-	response.Body.Close()
-	return contents
-}
 
 // Download downloads the file from the given URL and saves it into the given path.
 func Download(url, saveTo string) error {
